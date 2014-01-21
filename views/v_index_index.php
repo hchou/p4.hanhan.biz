aa9802279dd14385ef94e52a6b11aeb81a439baa
Hans H Chou <hchou@fas.harvard.edu>
2014-01-21 04:29:03 -0500

    Updating the descriptions for the project.

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,26 +1,28 @@
 <h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
 
 <article>
-
-    <h3>+1 Features</h3>
-    <h4>User Profile Update</h4>
+    
+    <h3>PROJECT 4</h3>
+    <h4>
+        This is the chat-enhanced version of the Blubber blogging application.
+        Authenticated users will be able to interact with others in realtime.
+        The chat uses the Pusher WebSocket to facilitate the interaction.
+        Technologies utilized include the core framework, PHP, JavaScript, and
+        AJAX.
+    </h4>
+    
+    <h4>
+        FEATURES
+    </h4>
 
     <ul>
 	    <li>Verify 'current password' is correct before proceeding</li>
 	    <li>Verify 'new email' isn't already in DB</li>
 	    <li>Update only modified user information</li>
-    </ul>
-	
-    <h4>User Delete</h4>
-    <ul>
     	<li>Admin/Superuser are allowed to delete other users</li>
-	    <li>User cannot her/himself</li>
-    </ul>
-
-    <h4>Delete Posts</h4>
-
-    <ul>
-	    <li>Admin/Superuser are allowed to delete posts</li>   
+	    <li>User cannot delete her/himself</li>
+        <li>Admin/Superuser are allowed to delete posts</li>
+        <li>Real-time chat for authenticated users</li>
     </ul>
 
     <h3>Sample Admin Email: i.byrnison@example.com Password: password</h3>
